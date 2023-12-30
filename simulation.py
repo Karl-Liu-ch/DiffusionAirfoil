c1b0e80dd0913b2655c5f09e0cdcf7a9193e8ca3
Karl-Liu-ch <liu_ht2010@qq.com>
2023-12-30 17:03:34 +0100

    update

--- simulation.py
+++ simulation.py
@@ -1,91 +1,8 @@
-#import os
-from __future__ import division
-import configparser
-import pexpect
-import wexpect
-#import subprocess as sp
-import gc
-
 import numpy as np
+from xfoil import XFoil
+from xfoil.model import Airfoil
+import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
-
-from utils import safe_remove, create_dir
-
-
-def compute_coeff(airfoil, reynolds=500000, mach=0, alpha=3, n_iter=200, tmp_dir='tmp'):
-    
-    create_dir(tmp_dir)
-    
-    gc.collect()
-    safe_remove('{}/airfoil.log'.format(tmp_dir))
-    fname = '{}/airfoil.dat'.format(tmp_dir)
-    with open(fname, 'wb') as f:
-        np.savetxt(f, airfoil)
-    
-    try:
-        # Has error: Floating point exception (core dumped)
-        # This is the "empty input file: 'tmp/airfoil.log'" warning in other approaches
-        child = wexpect.spawn('xfoil.exe')
-        timeout = 10
-        child.expect('XFOIL   c> ', timeout)
-        child.sendline('load {}/airfoil.dat'.format(tmp_dir))
-        child.expect('Enter airfoil name   s> ', timeout)
-        child.sendline('af')
-        child.expect('XFOIL   c> ', timeout)
-        child.sendline('OPER')
-        child.expect('.OPERi   c> ', timeout)
-        child.sendline('VISC {}'.format(reynolds))
-        child.expect('.OPERv   c> ', timeout)
-        child.sendline('ITER {}'.format(n_iter))
-        child.expect('.OPERv   c> ', timeout)
-        child.sendline('MACH {}'.format(mach))
-        child.expect('.OPERv   c> ', timeout)
-        child.sendline('PACC')
-        child.expect('Enter  polar save filename  OR  <return> for no file   s> ', timeout)
-        child.sendline('{}/airfoil.log'.format(tmp_dir))
-        child.expect('Enter  polar dump filename  OR  <return> for no file   s> ', timeout)
-        child.sendline()
-        child.expect('.OPERva   c> ', timeout)
-        child.sendline('ALFA {}'.format(alpha))
-        child.expect('.OPERva   c> ', timeout)
-        child.sendline()
-        child.expect('XFOIL   c> ', timeout)
-        child.sendline('quit')
-        
-        # child.expect(wexpect.EOF)
-        child.close()
-    
-        res = np.loadtxt('{}/airfoil.log'.format(tmp_dir), skiprows=12)
-        # if len(res) == 9:
-        #     CL = res[1]
-        #     CD = res[2]
-        # else:
-        #     CL = np.nan
-        #     CD = np.nan
-        CL = res[1]
-        CD = res[2]
-            
-    except Exception as ex:
-        print(ex)
-        print('XFoil error!')
-        CL = np.nan
-        CD = np.nan
-        
-    safe_remove(':00.bl')
-    
-    return CL, CD
-
-def read_config(config_fname):
-    
-    # Airfoil operating conditions
-    Config = configparser.ConfigParser()
-    Config.read(config_fname)
-    reynolds = float(Config.get('OperatingConditions', 'Reynolds'))
-    mach = float(Config.get('OperatingConditions', 'Mach'))
-    alpha = float(Config.get('OperatingConditions', 'Alpha'))
-    n_iter = int(Config.get('OperatingConditions', 'N_iter'))
-    
-    return reynolds, mach, alpha, n_iter
 
 def detect_intersect(airfoil):
     # Get leading head
@@ -105,13 +22,16 @@ def detect_intersect(airfoil):
     else:
         return False
 
-def evaluate(airfoil, config_fname='op_conditions.ini', return_CL_CD=False):
-    
-    # Read airfoil operating conditions from a config file
-    reynolds, mach, alpha, n_iter = read_config(config_fname)
-    
+def evaluate(airfoil, return_CL_CD=False):
+        
     if detect_intersect(airfoil):
         print('Unsuccessful: Self-intersecting!')
+        perf = np.nan
+        CL = np.nan
+        CD = np.nan
+        
+    elif airfoil[:,1].max() - airfoil[:,1].min() < 0.05:
+        print('Unsuccessful: Too thin!')
         perf = np.nan
         CL = np.nan
         CD = np.nan
@@ -123,37 +43,29 @@ def evaluate(airfoil, config_fname='op_conditions.ini', return_CL_CD=False):
         CD = np.nan
         
     else:
+        xf = XFoil()
+        xf.airfoil = Airfoil(airfoil[:,0], airfoil[:,1])
+        xf.Re = 4.5e4
+        xf.max_iter = 200
+        a, cl, cd, cm, cp = xf.aseq(0, 6, 0.5)
+        # cl, cd, cm, cp = xf.a(2)
+        perf = (cl/cd).max()
         
-        CL, CD = compute_coeff(airfoil, reynolds, mach, alpha, n_iter)
-        perf = CL/CD
-        
-        if perf < -100 or perf > 300 or CD < 1e-3:
-            print('Unsuccessful:', CL, CD, perf)
+        if perf < -100 or perf > 300 or cd.min() < 1e-3:
+            print('Unsuccessful:', cl.max(), cd.min(), perf)
             perf = np.nan
         elif not np.isnan(perf):
             print('Successful: CL/CD={:.4f}'.format(perf))
             
-#    if np.isnan(perf):
-#        from matplotlib import pyplot as plt
-#        plt.plot(airfoil[:,0], airfoil[:,1], 'o-', alpha=.5)
-#        plt.show()
-        
     if return_CL_CD:
-        return perf, CL, CD
+        return perf, cl.max(), cd.min()
     else:
         return perf
-    
-    
+
 if __name__ == "__main__":
-    
     airfoil = np.load('data/airfoil_interp.npy')
     # airfoil = np.load('sample.npy')
     # airfoil = np.squeeze(airfoil, axis=1)
-    airfoil = airfoil[np.random.choice(airfoil.shape[0])]
-    
-    # Read airfoil operating conditions from a config file
-    config_fname = 'op_conditions.ini'
-    reynolds, mach, alpha, n_iter = read_config(config_fname)
-    
-    CL, CD = compute_coeff(airfoil, reynolds, mach, alpha, n_iter)
-    print(CL, CD, CL/CD)
+    airfoil = airfoil[2]
+    perf = evaluate(airfoil)
+    print(perf)
